770c9669eaf6c3d2ddc6ab50caaa753d514ccd4f
hy3592 <hy3592@likelion.org>
2021-07-13 11:12:11 +0900

    7/13 4ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -14,8 +14,11 @@ def hello_world(request):
         new_model.text = temp
         new_model.save()
 
+        data_list = NewModel.objects.all()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'new_model':new_model})
+                      context={'new_model':data_list})
     else :
+        data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'text':'GET METHOD'})
+                      context={'new_model': data_list})
