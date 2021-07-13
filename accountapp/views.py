caed19745e9437a4f77bd50f53017951b9947b55
hy3592 <hy3592@likelion.org>
2021-07-13 11:36:50 +0900

    7/13 4ban class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import NewModel
 
 
@@ -14,11 +16,9 @@ def hello_world(request):
         new_model.text = temp
         new_model.save()
 
-        data_list = NewModel.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
-        return render(request, 'accountapp/hello_world.html',
-                      context={'new_model':data_list})
     else :
         data_list = NewModel.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'new_model': data_list})
+                      context={'data_list': data_list})
