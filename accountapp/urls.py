448103a6926c9435ac317557de0f10aad0d60c6e
hy3592 <hy3592@likelion.org>
2021-07-14 18:09:08 +0900

    7/1 4ban class commit 3

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreateView
@@ -8,7 +8,7 @@ app_name = 'accountapp'
 urlpatterns = (
     path('hello_world/', hello_world, name='hello_world'),
 
-    path('login/',LoginView.as_view(templte_name='accountapp/login.html'),name='login'),
+    path('login/',LoginView.as_view(template_name='accountapp/login.html'),name='login'),
 
     path('logout/',LogoutView.as_view(),name='logout'),
 
--- gis_4ban_1/settings.py
+++ gis_4ban_1/settings.py
@@ -14,6 +14,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.urls import reverse_lazy
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env_list = dict()
