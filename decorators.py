0c1e573f43dbd27e2c3570a079792d6edcb7532a
hy3592 <hy3592@likelion.org>
2021-07-21 18:17:20 +0900

    7/21 4ban class commit 4

--- decorators.py
+++ decorators.py
@@ -2,9 +2,9 @@
 
 def decorator(func):
     def decorated(input_text):
-        print('함수 시작!')
+        print('함수 시작!') #함수 이전에 실행되는 구문 /함수의 앞을 꾸며줌
         func(input_text)
-        print('함수 끝!')
+        print('함수 끝!') #함수 이후에 실행되는 구문 /함수의 뒤를 꾸며줌
     return decorated
 
 
@@ -15,3 +15,26 @@ def hello_world(input_text):
 
 
 hello_world('Hello World!')
+
+
+
+
+
+def tri_area(tri):
+    tri = (a * b) / 2
+    return tri
+
+def squ_area(squ):
+    squ = a * b
+    return squ
+a = int(input())
+b = int(input())
+
+
+def decorator(func):
+    def decorated(tri, squ):
+        if a >=0 and b >= 0:
+            return func(tri, squ)
+        else :
+            return ValueError('Input must be positive value')
+
